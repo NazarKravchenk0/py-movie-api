167ef94ffeb8f5cb776737ebf7ddc28cd46a90c6
nazarkravchenko <nazarnazarik5@gmail.com>
2026-01-28 14:23:47 +0200

    check2

--- cinema_service/cinema/views.py
+++ cinema_service/cinema/views.py
@@ -1,15 +1,15 @@
+from django.http import HttpRequest
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.request import Request
 from rest_framework.response import Response
 
 from cinema.models import Movie
 from cinema.serializers import MovieSerializer
 
 
-@api_view(('GET', 'POST'))
-def movie_list(request: Request) -> Response:
+@api_view(['GET', 'POST'])
+def movie_list(request: HttpRequest) -> Response:
     if request.method == 'GET':
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
@@ -21,8 +21,8 @@ def movie_list(request: Request) -> Response:
     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(('GET', 'PUT', 'DELETE'))
-def movie_detail(request: Request, pk: int) -> Response:
+@api_view(['GET', 'PUT', 'DELETE'])
+def movie_detail(request: HttpRequest, pk: int) -> Response:
     movie = get_object_or_404(Movie, pk=pk)
 
     if request.method == 'GET':
